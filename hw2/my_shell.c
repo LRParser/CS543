7dd468f29d388c1d3f3f10e7c0e1a06dba78d94f
Joseph Heenan <jth82@drexel.edu>
2018-01-24 12:54:24 +0100

    Make sure to NULL out array properly

--- hw2/my_shell.c
+++ hw2/my_shell.c
@@ -2,30 +2,34 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 #define MAX_LINE 80
 
 int main(void)
 {
   const int args_max_size = MAX_LINE/2 + 1;
+  char input_text[args_max_size];
   char *args[args_max_size];
   int should_run = 1;
   char *delimiters = " ";
   int i = 0;
-  int args_idx = 0;
-  pid_t pid;
   while (should_run)
   {
     i = 0;
-    args_idx = 0;
+    //args_idx = 0;
     printf("osh>\n");
     fflush(stdout);
-    char input_text[80];
-    fgets(input_text,80,stdin); // = "Test one two";
+    gets(input_text);
+//    fgets(input_text,args_max_size,stdin); // = "Test one two";
+    // We need to null-terminate the input_text
+//    input_text[strlen(input_text) -1] = '\0';
     //gets(input_text);
     printf("%s\n",input_text);
     char* word;
     // Note: Referred to need to include <string.h> here: https://gist.github.com/efeciftci/9120921
     word = strtok(input_text, " ");
+    int args_idx = 0;
     args[args_idx] = word;
     args_idx++;
     printf("Token: %s\n", word);
@@ -34,18 +38,19 @@ int main(void)
       args[args_idx] = word;
       args_idx++;
     }
+    args[args_idx] = NULL;
 
     for(;i<args_idx;i++)
     {
       printf("Parsed: %s\n",args[i]);
     }
     i = args_idx + 1;
-    for(; i < args_max_size;i++)
+    for(; args_idx < args_max_size;args_idx++)
     {
-      //args[i] = NULL;
+      args[args_idx] = NULL;
     }
     // Now let's fork a process
-    pid = fork();
+    pid_t pid = fork();
     if (pid < 0)
     {
       fprintf(stderr,"Error while trying to fork\n");
@@ -54,9 +59,11 @@ int main(void)
     {
       // This is the child process. Run the job
       printf("Running the job\n");
+      
+      printf("%s\n",args[0]);
+      //execlp("/bin/ls","ls",NULL);
       execvp(args[0],args);
-      should_run = 0;
-      continue;
+      exit(errno);
     }
     else
     {
