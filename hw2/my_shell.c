4c77a22082e50dbb980aca8e91b6767388b25e0b
Joseph Heenan <jth82@drexel.edu>
2018-02-04 23:06:34 +0100

    Alias not working

--- hw2/my_shell.c
+++ hw2/my_shell.c
@@ -7,28 +7,29 @@
 #include <sys/wait.h>
 #include <errno.h>
 #define MAX_LINE 80
-#define DEBUG 0
+#define DEBUG 1
 
 int main(void)
 {
- int args_max_size = MAX_LINE/2 + 1;
- char *args[41];
- int args_idx;
+ const int args_max_size = MAX_LINE/2 + 1;
+ //int args_idx;
  int should_run = 1;
  char *delimiters = " ";
- const int history_size = 3;
+ const int history_size = 10;
  char command_history[history_size][args_max_size];
+ char alias_tos[history_size][args_max_size];
+ char alias_froms[history_size][args_max_size];
  int commands_run = 0;
+ int aliases_set = 0;
  while (should_run)
  {
-  int i;
   char input_text[args_max_size];
   int repeat_history_command = 0;
-  i = 0;
   printf("osh>");
   fflush(stdout);
   fgets(input_text,sizeof input_text,stdin);
-  printf("Got: %s\n",input_text);
+  if(DEBUG)
+    printf("Got: %s\n",input_text);
   // Remove the newline
   input_text[strcspn(input_text,"\n")] = '\0';
   
@@ -77,6 +78,7 @@ int main(void)
   char input_text_copy[args_max_size];
   // Before we tokenize our input string, we make a copy for history storage, as tokenization mutates original string
   strcpy(input_text_copy,input_text);
+
   int args_idx = 0;
   char* args[args_max_size];
   char* word;
@@ -96,7 +98,6 @@ int main(void)
   }
   else if(strcmp("history",args[0]) == 0)
   {
-    printf("Print history\n");
     int l;
     l = commands_run < history_size ? commands_run : history_size;
     for(;l>0;l--)
@@ -105,7 +106,54 @@ int main(void)
     }
     continue;
   }
-  printf("Later New program: %s\n",args[0]);
+  else if(strcmp("alias",args[0]) == 0)
+  {
+    if(args_idx < 2)
+    {
+      printf("You must provide at least three arguments to an alias command\n");
+      printf("For instance, alias dir = \"ls -la\"\n");
+      continue;
+    }
+    else if(aliases_set >= history_size)
+    {
+      printf("This shell only supports setting %d aliases, please restart it to set new ones\n",history_size);
+      continue;
+    }
+    else
+    {
+      char alias_from[args_max_size / 2];
+      char alias_to[args_max_size / 2];
+      strcpy(alias_from,args[1]);
+      char arg_parsed[args_max_size];
+      int parsed_idx = 0;
+      for(int i=2; i < args_idx; i++)
+      {
+         size_t arg_len = strlen(args[i]);
+         for(int j=0; j < arg_len; j++)
+         {
+           if (args[i][j] != '"')
+           {
+             arg_parsed[parsed_idx] = args[i][j];
+             parsed_idx++;
+           }
+         }
+
+         if(i < args_idx - 1)
+         {
+            arg_parsed[parsed_idx] = ' ';
+            parsed_idx++;
+         }
+
+         // Append string
+
+      }
+      arg_parsed[parsed_idx] = '\0';
+      strcpy(alias_froms[aliases_set],alias_from);
+      strcpy(alias_tos[aliases_set],arg_parsed);
+      aliases_set++;
+      continue;
+    }
+  }
 
   if (!repeat_history_command)
   {
@@ -127,12 +175,12 @@ int main(void)
     idx_written = commands_run;
     strcpy(command_history[idx_written],input_text_copy);
   }
-  printf("Added to history at idx: %d, %s\n",idx_written,command_history[idx_written]);
   commands_run++;
   }
 
 
-  printf("Command to now run: %s\n",args[0]);
+  if (DEBUG)
+    printf("Command to now run: %s\n",args[0]);
 
   int should_wait = 0;
   int path_provided = 0;
@@ -142,22 +190,19 @@ int main(void)
   should_wait = (strcmp("&",args[args_idx-1]) != 0);
   path_provided = args[0][0] == '/';
   set_command = strcmp("set",args[0]) == 0;
-  printf("First char: %c\n",args[0][0]);
-  printf("Should wait: %d\n",should_wait);
-  printf("Path provided: %d\n",path_provided);
-  printf("Set command: %d\n",set_command);
   if (set_command && args_idx >= 2)
   {
     if(strcmp("path",args[1]) == 0 && strcmp("=",args[2]) == 0)
     {
       char desired_path[1024];
       const char *path_start = "PATH=";
-      strncpy(desired_path,"PATH=",sizeof(path_start));
-      for(i = 3; i < args_idx; i++)
+      strcpy(desired_path,"PATH=");
+      for(int i = 3; i < args_idx; i++)
       {
-       char* path_segment = args[i];
+       char path_segment[args_max_size];
+       strcpy(path_segment,args[i]);
        printf("Segment is: %s\n",path_segment);
-       char path_segment_processed[1024];
+       char path_segment_processed[2 * args_max_size];
        // We validate that the first segment contains a left paren, and remove it
        int new_char_idx = 0;
        int j=0;
@@ -167,7 +212,7 @@ int main(void)
        {
          if(path_segment[j] == '.')
          {
-           char cwd[1024];
+           char cwd[4 * args_max_size];
            if(getcwd(cwd,sizeof(cwd)) != NULL)
            {
              printf("Resolve cwd as: %s\n",cwd);
@@ -176,6 +221,7 @@ int main(void)
              for(; k < cwd_len; k++)
              {
                path_segment_processed[new_char_idx] = cwd[k];
+               new_char_idx++;
              }
            }
          }
@@ -185,10 +231,9 @@ int main(void)
            new_char_idx++; 
          }
        }
+       path_segment_processed[new_char_idx] = '\0';
        j = 0;
-
-       printf("Adding to path: %s\n",args[i]);
-       strncat(desired_path,path_segment_processed,sizeof(path_segment_processed));
+       strcat(desired_path,path_segment_processed);
        if(i < args_idx - 1)
        {
          const char *path_sep = ":";
@@ -196,14 +241,11 @@ int main(void)
        }
       }
       char* pathvar = getenv("PATH");
-      printf("Initial pathvar=%s\n",pathvar);
-      printf("Full desired path: %s\n",desired_path);
-      //putenv(desired_path);
+      //printf("Initial pathvar=%s\n",pathvar);
+      //printf("Full desired path: %s\n",desired_path);
+      putenv(desired_path);
       pathvar = getenv("PATH");
-      printf("New pathvar=%s\n",pathvar);
-
-
-      i = 0;
+      //printf("New pathvar=%s\n",pathvar);
     }
     else {
       printf("I see you entered a set command, but it was not in the form set path = (path1 path2)\n");
@@ -211,21 +253,69 @@ int main(void)
       continue;
     }
   }
-  args[args_idx] = NULL;
+  //args[args_idx] = NULL;
 
-  for(;i<args_idx;i++)
+  for(int i = 0;i<args_idx;i++)
   {
    printf("Parsed: %s\n",args[i]);
   }
-  i = 0;
+
+  // See if the program name has been aliases by anything
+  int match_found = 0;
+  int args_idx_new = 0;
+  char* args_new[args_max_size];
+  for(int i = 0; i < aliases_set; i++)
+  {
+    if(!match_found && strcmp(alias_froms[i],args[0]) == 0)
+    {
+      printf("Match found %s\n",alias_tos[i]);
+      char alias_to_tokenize[args_max_size];
+      strcpy(alias_to_tokenize,alias_tos[i]);
+      //alias_to_tokenize[strlen(alias_tos[i])] = '\0';
+      char* word;
+      word = strtok(alias_to_tokenize, " ");
+      args_new[args_idx_new] = word;
+      args_idx_new++;
+      while((word = strtok(NULL, " ")) != NULL)
+      {
+       args_new[args_idx_new] = word;
+       args_idx_new++;
+      }
+      match_found = 1;
+      printf("args_idx_new is: %d\n",args_idx_new);
+    }
+  }
+
+  if (match_found)
+  {
+      for(int i = 0; i < args_idx_new; i++)
+      {
+        strcpy(args[i],args_new[i]);
+      }
+      args_idx = args_idx_new;
+  }
+  printf("args_idx is: %d\n",args_idx);
+
+  //args[args_idx] = NULL;
+
+  for(int i = 0;i<args_idx;i++)
+  {
+   printf("Args: %s\n",args[i]);
+  }
+
   if (args_idx >= 2 && strcmp("&",args[args_idx -1]) == 0)
   {
     printf("Strip last character%s\n",args[args_idx-1]);
     args[args_idx-1] = NULL;
   }
-  for(; args_idx < args_max_size;args_idx++)
+  for(int i = args_idx; i < args_max_size;i++)
   {
-   args[args_idx] = NULL;
+   args[i] = NULL;
+  }
+
+  for(int i = 0; i < args_idx; i++)
+  {
+    printf("Last hurrah: %s\n",args[i]);
   }
 
   // Here, we fork
@@ -236,20 +326,35 @@ int main(void)
   }
   else if (pid == 0)
   {
-   // This is the child process. Run the job
-   printf("Running the job\n");
-   printf("%s\n",args[0]);
+   // Here this is the child. In the context of the child process, run the command
    int ret;
-   ret = execvp(args[0],args);
+   printf("Running the job: \n");
+   if (match_found)
+   {
+     printf("%s\n",args_new[0]);
+     ret = execvp(args_new[0],args_new);
+   }
+   else
+   {
+     printf("%s\n",args[0]);
+     ret = execvp(args[0],args);
+   }
    if(ret != 0)
    {
-     printf("Unable to find the command: %s in the PATH\n",args[0]);
+     if (match_found)
+     {
+       printf("Unable to find the command: %s in the PATH\n",args_new[0]);
+     }
+     else
+     {
+       printf("Unable to find the command: %s in the PATH\n",args[0]);
+     }
    }
    exit(errno);
   }
   else
   {
-   // This is the parent. Wait for the child to finish
+   // Here this is the parent. Wait for child to finish, unless & was given
    if(should_wait)
    {
      wait(NULL);
