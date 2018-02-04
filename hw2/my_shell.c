6720ef19257b354e51875aa62bbceeaf724ded83
Joseph Heenan <jth82@drexel.edu>
2018-02-04 14:03:05 +0100

    Path parsing logic

--- hw2/my_shell.c
+++ hw2/my_shell.c
@@ -13,13 +13,9 @@ int main(void)
   char input_text[args_max_size];
   char *args[args_max_size];
 
-  char *paths_to_search[args_max_size];
-  char *pathvar;
   int should_run = 1;
   char *delimiters = " ";
-  int i = 0;
-  int set_path_command_run = 0;
-  int paths_to_search_len = 0;
+  int i;
   while (should_run)
   {
     i = 0;
@@ -55,24 +51,41 @@ int main(void)
     {
         if(strcmp("path",args[1]) == 0 && strcmp("=",args[2]) == 0)
         {
-            set_path_command = 1;
-            char* desired_path[1024];
-            strcat(desired_path,"PATH=");
-            for(i = 2; i < args_idx; i++)
+            char desired_path[1024];
+            const char *path_start = "PATH=";
+            strncpy(desired_path,"PATH=",sizeof(path_start));
+            for(i = 3; i < args_idx; i++)
             {
-               paths_to_search[i-2] = args[i];
-               printf("Adding to path: %s\n",args[i]);
-               strcat(desired_path,args[i]);
-               strcat(desired_path,":");                
+               char* path_segment = args[i];
+               printf("Segment is: %s\n",path_segment);
+               char path_segment_processed[30];
+               // We validate that the first segment contains a left paren, and remove it
+               int new_char_idx = 0;
+               int j=0;
+               int path_segment_len = strlen(path_segment);
+               printf("Segment len is: %d\n",path_segment_len);
+               for(;j<path_segment_len;j++)
+               {
+                   if(path_segment[j] != '(' && path_segment[j] != ')')
+                   {
+                      path_segment_processed[new_char_idx] = path_segment[j];
+                      new_char_idx++; 
+                   }
+               }
+               j = 0;
 
-               printf("In search list: %s\n",paths_to_search[i-2]);
-               dirs_in_path++;
-               paths_to_search_len++;
+               printf("Adding to path: %s\n",args[i]);
+               strncat(desired_path,path_segment_processed,sizeof(path_segment_processed));
+               if(i < args_idx - 1)
+               {
+                   const char *path_sep = ":";
+                   strcat(desired_path,path_sep);                
+               }
             }
-            pathvar = getenv("PATH");
+            char* pathvar = getenv("PATH");
             printf("Initial pathvar=%s\n",pathvar);
             printf("Full desired path: %s\n",desired_path);
-            putenv(desired_path);
+            //putenv(desired_path);
             pathvar = getenv("PATH");
             printf("New pathvar=%s\n",pathvar);
 
@@ -81,7 +94,8 @@ int main(void)
         }
         else {
             printf("I see you entered a set command, but it was not in the form set path = (path1 path2)\n");
-            printf("Please resubmit");
+            printf("Please resubmit\n");
+            continue;
         }
     }
     args[args_idx] = NULL;
@@ -96,21 +110,6 @@ int main(void)
       args[args_idx] = NULL;
     }
 
-    // If we set a path, we now search for the executable in each of the path locations
-    if (set_path_command_run)
-    {
-        for(;i<paths_to_search_len;i++)
-        {
-            char *program_name = args[0];
-            char str[80];
-            strcpy(str,program_name);
-            strcat(str,"/");
-            strcat(str, paths_to_search[i]);
-            printf("Searching in path: %s\n",str);
-
-        }
-        i = 0; 
-    }
     // Here, we fork
     pid_t pid = fork();
     if (pid < 0)
@@ -130,10 +129,6 @@ int main(void)
       // This is the parent. Wait for the child to finish
       wait(NULL);
       printf("Command run, waiting for next command\n");
-      if(set_path_command)
-      {
-          set_path_command_run = 1;
-      }   
     }
     
   }
