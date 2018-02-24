e302a7ff2453e680dec43009e9fa06602be5abdb
Joe <jth82@drexel.edu>
2018-02-24 11:14:12 +0100

    Mapping locations from file

--- hw3/memory_manager.c
+++ hw3/memory_manager.c
@@ -15,8 +15,17 @@ int main()
   int frames_written = 0;
   int pages_written = 0;
 
-  uint8_t page_table_keys[page_table_num_entries];
-  uint8_t page_table_values[page_table_num_entries];
+  uint8_t page_table_keys[num_pages];
+  uint8_t page_table_values[num_pages];
+  char physical_memory[num_frames][frame_size];
+
+
+  // Initialize all page table keys and values to 0
+  for(int i=0; i<num_pages; i++)
+  {
+    page_table_keys[i] = 0;
+    page_table_values[i] = 0;
+  }
 
   // This implies each page table entry can store a single byte
 
@@ -33,40 +42,55 @@ int main()
     uint8_t page_num = mid_8;
     uint8_t offset_num = low_8;
     
-    uint16_t phy_addr = frames_written * frame_size + offset_num;
 
-    int frame_num;
+    int frame_num = 0;
     int page_table_hit = 0;
+    char* frame_contents;
 
-    // Try to find this page in the page table. 
+    // First, try to get the frame for the page_num from TLB
+    // To be implemented
+
+
+    // If this fails, see if we can get the frame for the page_num from page table
 
     for (int i=0; i < num_pages; i++)
     {
       if(page_table_keys[i] == page_num)
       {
         frame_num = page_table_values[i];
+        frame_contents = physical_memory[frame_num];
         page_table_hit = 1;
       }
     }
 
+    // If this also fails, a page fault occurs. In this case, we read a 256-byte page (at page_num) from the backing store
     if (!page_table_hit)
     {
       frame_num = frames_written;
+
+      FILE *fp;
+      if ((fp = fopen("BACKING_STORE.bin","rb")) != NULL)
+      {
+        fseek(fp, sizeof(char)*frame_size*page_num, SEEK_SET);
+        fread(frame_contents, sizeof(char), frame_size, fp);
+        fclose(fp);
+      }
+
       frames_written++;
     }
 
     page_table_keys[pages_written] = page_num;
     page_table_values[pages_written] = frame_num;
 
-    printf("Virtual address: %d, page_num: %d, offset: %d, frames_num: %d, physical address: %d\n",v_addr,page_num, offset, frame_num, phy_addr);
+    uint16_t phy_addr = frame_num * frame_size + offset_num;
 
-    frames_written++;
+    char byte_at_addr = frame_contents[offset_num];
 
-    // First, try to get the frame for the page_num from TLB
+    printf("Virtual address: %d, page_num: %d, frame_num: %d, offset: %d, physical address: %d, value: %d\n",v_addr,page_num, frame_num, offset_num, phy_addr, byte_at_addr);
 
-    // If this fails, see if we can get the frame for the page_num from page table
 
-    // If this also fails, a page fault occurs. In this case, we read a 256-byte page (at page_num) from backing
+
+
     // storage, and store it in a page frame in memory.
 
     //int page = buf_
